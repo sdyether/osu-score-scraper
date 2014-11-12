be27dc98d6c3adc317e5527f7c1e020fb2f91fb6
Shane Dyer <sdye1991@gmail.com>
2014-11-12 22:19:15 +1100

    First functioning commit.

--- page.rb
+++ page.rb
@@ -1,7 +1,7 @@
 class Page
 
 	def parse_raw
-	
+		#onclick appears for each record only
 	end
 	
 	def initialize
@@ -10,12 +10,16 @@ class Page
 		@players = Array.new(50)
 		@ranks = Array.new(50)
 		@ranked_scores = Array.new(50)
-		@accs = Array.new(50)
+		@accuracies = Array.new(50)
 		@total_scores = Array.new(50)
 		@levels = Array.new(50)
 		@play_counts = Array.new(50)
 		@SS_counts = Array.new(50)
 		@S_counts = Array.new(50)
 		@A_counts = Array.new(50)
+		@countries = Array.new(50) #TODO tricky to parse
 	end
+	
+	#getters and setters
+	attr_accessor :raw_html, :players, :ranks, :ranked_scores, :accuracies, :total_scores, :levels, :play_counts, :SS_counts, :S_counts, :A_counts, :countries
 end
--- ranked_score_scraper.rb
+++ ranked_score_scraper.rb
@@ -5,13 +5,22 @@ class RankedScoreScraper
 
 	def start()
 		#main scraper method
-		self.setup
+		setup()
 		
 		cur_page = 1
 		while cur_page <= @num_pages
-			get_page(cur_page)
-			cur_page++
+		
+			@pages[cur_page - 1] = get_page(cur_page)
+			if !(@pages[cur_page - 1])
+				abort( "Error: page " + cur_page.to_s + " could not be retrieved.  Exiting." )
+			end
+			
+			cur_page += 1
+			sleep(60/@max_requests_per_min)
 		end
+		
+		puts @num_pages.to_s + " pages successfully scraped."
+		puts @pages[0].raw_html
 		
 	end
 	
@@ -19,12 +28,12 @@ class RankedScoreScraper
 	def get_page(num)
 		
 		attempt_no = 1
-		#max_page_attempts = 4
-		#intial_backoff_wait = 30 #seconds
 		
-		while attempt_no <= @max_page_attempts
+		while (attempt_no <= @max_page_attempts)
 		
 			#try to get a page
+			puts "Grabbing page " + num.to_s + " ..."
+			
 			url = @base_url + num.to_s
 			
 			url = URI.parse(url)
@@ -32,36 +41,37 @@ class RankedScoreScraper
 			response = Net::HTTP.start(url.host, url.port) do |http|
 				http.request(req)
 			end
-			
+
 			if response.code == "200"
 				page = Page.new
 				page.raw_html = response.body
-				return page
+				puts "Success."
+				return page #success
 			end
 			
-			sleep(@intial_backoff_wait * attempt_no)
-			attempt_no++
+			sleep(@intial_backoff_wait * attempt_no) #binary backoff
+			attempt_no += 1
 			#try again
 		end
 		
-		return nil
+		return nil #failed
 	end
 	
 	def setup()
 		#TODO get these from config file instead
 		@num_pages = 1
-		@requests_per_min = 10
-		@base_url = "https://osu.ppy.sh/p/playerranking/?m=0&s=3&o=1&page="
+		@max_requests_per_min = 10
+		@base_url = "http://osu.ppy.sh/p/playerranking/?m=0&s=3&o=1&page="
 		@intial_backoff_wait = 30
 		@max_page_attempts = 4
 		@records_per_page = 50
 		
-		@pages = Array.new(@num_pages) { Page.new }
+		@pages = Array.new(@num_pages)
 	end
 	
 	def initialize
 		@num_pages #how many pages to scrape
-		@requests_per_min #max server requests per minute
+		@max_requests_per_min #be kind to server
 		@base_url
 		@finished = false
 		@intial_backoff_wait
@@ -71,6 +81,9 @@ class RankedScoreScraper
 		#let's keep all pages in memory so we can rollback easier on failed scrape
 		@pages
 	end
+	
+	#getters and setters 
+	attr_accessor :num_pages, :max_requests_per_min, :base_url, :finished, :intial_backoff_wait, :max_page_attempts, :records_per_page, :pages
 	
 end
 
